d183280b312290f588c1ee9ae3dc068a867579eb
SusmitaDutta1 <145219916+SusmitaDutta1@users.noreply.github.com>
2024-01-17 20:39:07 +0530

    Update Items.jsx

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -1,242 +1,48 @@
-import React, { useEffect, useState } from "react";
-import Item from "./Item";
-import { useAuth } from './username_auth';
-import {
-  collection,
-  deleteDoc,
-  doc,
-  onSnapshot,
-  query,
-  addDoc,
-  where,
-} from "firebase/firestore";
-import { db } from "../firebase";
-import { motion } from "framer-motion";
+import React, { useEffect } from "react";
 
-const Items = () => {
-  const { getCurrentUser}= useAuth();
-  const [items, setitems] = useState([]);
-  const [changed, setchanged] = useState([]);
-  const [search, setsearch] = useState("");
-  const [name, setname] = useState("");
-  const [date, setdate] = useState("");
-  const [tag, settag] = useState("food");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    
-    const user = await getCurrentUser();
-    const userId = user.uid;
-    // console.log(getCurrentUser.uid)
-
-    const docref = await addDoc(collection(db, "items"), { userId: userId, name, date, tag });
-
-    setname("");
-    setdate("");
-    settag("food");
-  };
-
-  const handleMed = () => {
-    let newArr = items.filter((item) => {
-      return item.tag.toLowerCase().includes("med");
-    });
-    setchanged(newArr);
-  };
-
-  const handleFood = () => {
-    let newArr = items.filter((item) => {
-      return item.tag.toLowerCase().includes("food");
-    });
-    setchanged(newArr);
-  };
-
-  const handleCosmetics = () => {
-    let newArr = items.filter((item) => {
-      return item.tag.toLowerCase().includes("cosmetics");
-    });
-    setchanged(newArr);
-  };
-
-  const handleAll = () => {
-    setchanged(items);
-  };
-
-  const handleDelete = async (id) => {
-    await deleteDoc(doc(db, "items", id));
-  };
-
-  // useEffect(() => {
-  //   const q = query(collection(db, "items"), where("userId","==",getCurrentUser.uid));
-  //   const unsubscribe = onSnapshot(q, (querySnapshot) => {
-  //     let itemArr = [];
-  //     querySnapshot.forEach((doc) => {
-  //       itemArr.push({ ...doc.data(), id: doc.id });
-  //     });
-  //     setitems(itemArr);
-  //     setchanged(itemArr);
-  //   });
-
-  //   return () => unsubscribe();
-  // }, [getCurrentUser]);
-
+const Item = ({ name, date, tag, id, handleDelete }) => {
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const user = await getCurrentUser();
-        // console.log("Current User ", user);
-  
-        if (!user) {
-          // console.log("No Current User.");
-          return;
-        }
-  
-        const userId = user.uid;
-        // console.log("User ID:", userId);
-  
-        const q = query(collection(db, "items"), where("userId", "==", userId));
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-          let itemArr = [];
-          querySnapshot.forEach((doc) => {
-            itemArr.push({ ...doc.data(), id: doc.id });
+    const curdate = new Date().toISOString();
+
+    if (curdate.slice(0, 10) === date) {
+      Notification.requestPermission().then((perm) => {
+        if (perm === "granted") {
+          new Notification(`Item: ${name}`, {
+            body: "It expires today! Throw it away, or it will throw you away :)",
           });
-          setitems(itemArr);
-          setchanged(itemArr);
-        });
-  
-        return () => {
-          // console.log("Unsubscribed from Items Snapshot");
-          unsubscribe();
-        };
-      } catch (error) {
-        // console.error("Error user:", error);
-      }
-    };
-  
-    fetchData();
+        }
+      });
+    }
   }, []);
+
+  const handle = () => {
+    Notification.requestPermission().then((perm) => {
+      if (perm === "granted") {
+        new Notification(`Item: ${name}`, {
+          body: "It expires today! Throw it away, or it will throw you away :)",
+        });
+      }
+    });
+  };
 
   return (
     <>
-      <div className="header">
-        <div className="searches">
-          <form className="search">
-            <input
-              type="text"
-              value={search}
-              onChange={(e) => setsearch(e.target.value)}
-              name="srch"
-              id="srch"
-              placeholder="Search ..."
-            />
-          </form>
-
-          <button data-bs-toggle="modal" data-bs-target="#create">
-            <div className="add">
-              <i className="fa-solid fa-plus"></i> <span>New</span>
-            </div>
-          </button>
-        </div>
-
-        <div className="filters">
-          <div onClick={handleAll} className="filter">
-            All
-          </div>
-          <div onClick={handleMed} className="filter">
-            Medicine
-          </div>
-          <div onClick={handleFood} className="filter">
-            Food
-          </div>
-          <div onClick={handleCosmetics} className="filter">
-            Cosmetics
+      <div className="item">
+        <div className="item-details">
+          <div className="item-name">{name}</div>
+          <div className="item-type">{tag}</div>
+          <div className="date">
+            <p className="date-head">Expires on</p>
+            <p className="date-head">{date}</p>
+            {/* <button onClick={handle}>Click</button> */}
           </div>
         </div>
-
-        <motion.div layout className="items">
-          {changed
-            .filter((item) => {
-              return search.toLowerCase() === ""
-                ? item
-                : item.name.toLowerCase().includes(search);
-            })
-            .map((item) => {
-              return (
-                <Item
-                  key={item.id}
-                  id={item.id}
-                  name={item.name}
-                  date={item.date}
-                  tag={item.tag}
-                  handleDelete={handleDelete}
-                />
-              );
-            })}
-        </motion.div>
-      </div>
-
-      <div className="modal" id="create" tabIndex="-1">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h4 className="modal-title">Add An Item</h4>
-            </div>
-            <div className="modal-body">
-              <form onSubmit={handleSubmit}>
-                <div className="input">
-                  <label htmlFor="name">Name</label>
-                  <input
-                    onChange={(e) => setname(e.target.value)}
-                    value={name}
-                    type="text"
-                    required={true}
-                    name="name"
-                    id="name"
-                  />
-                </div>
-                <div className="input">
-                  <label htmlFor="date">Expiry Date</label>
-                  <input
-                    onChange={(e) => setdate(e.target.value)}
-                    value={date}
-                    type="date"
-                    required={true}
-                    name="date"
-                    id="date"
-                  />
-                </div>
-                <div className="input">
-                  <label htmlFor="tag">Tag</label>
-                  <select
-                    className="text-center"
-                    name="tag"
-                    id="tag"
-                    value={tag}
-                    onChange={(e) => settag(e.target.value)}
-                  >
-                    <option value="food">Food</option>
-                    <option value="med">Medicine</option>
-                    <option value="cosmetics">Cosmetics</option>
-                  </select>
-                </div>
-                <div className="btns">
-                  <button type="submit" className="btn btn-success">
-                    Add Item
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-bs-dismiss="modal"
-                  >
-                    Close
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
+        <button onClick={() => handleDelete(id)} className="delete">
+          <i className="fa-regular fa-circle-xmark cross"></i>
+        </button>
       </div>
     </>
   );
 };
 
-export default Items;
+export default Item;
